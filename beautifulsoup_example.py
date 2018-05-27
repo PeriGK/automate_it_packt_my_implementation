5b54be2b32a34e343ac33f9addfefac5354ccb0f
Periklis Gkolias <per.gkolias@gmail.com>
2018-05-27 16:54:09 +0300

    Adding example of beautifulsoup

---
+++ beautifulsoup_example.py
@@ -0,0 +1,15 @@
+import bs4
+from os import getcwd
+myfile = open('automate_it_packt_my_implementation/python.html')
+soup = bs4.BeautifulSoup(myfile, "lxml")
+#Making the soup
+print ("BeautifulSoup Object:", type(soup))
+
+print(soup.find_all('a'))
+print(soup.find_all('strong'))
+print(soup.find('div', {'id': 'inventor'}))
+print(soup.select('#inventor'))
+print(soup.select('.wow'))
+print ("Facebook URL:", soup.find_all('a')[0]['href'])
+print ("Inventor:", soup.find('div', {"id":"inventor"}).text)
+print ("Span content:", soup.select('span')[0].getText())
