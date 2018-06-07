13d29dd73df2c6ce4f1d89d841aca3ce48e3a5a7
Periklis Gkolias <per.gkolias@gmail.com>
2018-06-07 20:39:27 +0300

    Added plot logic

--- parsing_csvs.py
+++ parsing_csvs.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
 
 pd.set_option('display.line_width', 5000)
 pd.set_option('display.max_columns', 60)
@@ -10,3 +12,9 @@ df = pd.read_csv('~/Downloads/TechCrunchcontinentalUSA.csv', index_col='fundedDa
 
 raised = df['raisedAmt'][:5]
 print('Funding raised by companies over time: \n', raised)
+
+sns.set_style('darkgrid')
+sns_plot = df['raisedAmt'].plot()
+plt.ylabel('Amount raised in USD')
+plt.xlabel('Funding year')
+plt.savefig('AmountRaisedOverTime.pdf')
