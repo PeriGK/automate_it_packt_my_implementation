3fa8ea7489dac40914aa3863499fb818bdf3fdc8
Periklis Gkolias <per.gkolias@gmail.com>
2018-06-07 20:48:48 +0300

    An arbitrary set of 20 companies with their category and funding along with an aggregation of category counts

---
+++ csvs_multiple_columns.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+
+pd.set_option('display.line_width', 5000)
+pd.set_option('display.max_columns', 60)
+
+fundings = pd.read_csv('~/Downloads/TechCrunchcontinentalUSA.csv')
+print('Type of funding:\n ',fundings[:5]['round'])
+
+# print('First five rows {}'.format(df[:5]))
+
+print('Selected company category and date of funding \n', fundings[
+    ['company', 'category', 'fundedDate']
+][800:820])
+
+counts = fundings['category'].value_counts()
+print('Most common categories of companies that raised funds: ', counts)
